a39a946a2be0aeddc880af47dc42d082f53a68c5
Dizon-Joel <joeldizon111111@gmail.com>
2025-02-26 06:02:23 +0800

    category nav

--- lib/category.dart
+++ lib/category.dart
@@ -14,72 +14,91 @@ class _CategoryAppState extends State<CategoryApp> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-        child: SingleChildScrollView(
-      child: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 180,
+        navigationBar: CupertinoNavigationBar(
+          leading: CupertinoButton(
+              padding: EdgeInsets.zero,
+              child: Icon(
+                CupertinoIcons.left_chevron,
+                size: 20,
+                color: Color(0xFF1A120B),
               ),
-              Text('Discover Daily Inspiration and Motivation',
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
-                  textAlign: TextAlign.center),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-                child: Container(
-                  width: double.maxFinite,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFE5E5CB),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: CupertinoButton(
-                      child: Text(
-                        'Quotes',
-                        style: TextStyle(color: CupertinoColors.black),
-                      ),
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                            context,
-                            CupertinoPageRoute(
-                                builder: (context) => CategoryQuote()));
-                      }),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-                child: Container(
-                  width: double.maxFinite,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFE5E5CB),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: CupertinoButton(
-                      child: Text(
-                        'Verses',
-                        style: TextStyle(color: CupertinoColors.black),
-                      ),
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                            context,
-                            CupertinoPageRoute(
-                                builder: (context) => VerseCategory()));
-                      }),
-                ),
-              ),
-            ],
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    CupertinoPageRoute(builder: (context) => MyApp()),
+                    (Route<dynamic> route) => false);
+              }),
+          middle: Text(
+            'Mon chéri',
+            style: TextStyle(color: Color(0xFF1A120B)),
           ),
         ),
-      ),
-    ));
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 180,
+                  ),
+                  Text('Discover Daily Inspiration and Motivation',
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                      textAlign: TextAlign.center),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+                    child: Container(
+                      width: double.maxFinite,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFE5E5CB),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: CupertinoButton(
+                          child: Text(
+                            'Quotes',
+                            style: TextStyle(color: CupertinoColors.black),
+                          ),
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                                context,
+                                CupertinoPageRoute(
+                                    builder: (context) => CategoryQuote()));
+                          }),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+                    child: Container(
+                      width: double.maxFinite,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFE5E5CB),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: CupertinoButton(
+                          child: Text(
+                            'Verses',
+                            style: TextStyle(color: CupertinoColors.black),
+                          ),
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                                context,
+                                CupertinoPageRoute(
+                                    builder: (context) => VerseCategory()));
+                          }),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
